b946ab8a5c8b666e3fd2ed3a412cb0c4ae9db8e3
juliaanholcer <julia.anholcer@interia.pl>
2017-11-28 13:41:37 +0100

    Commit with TODO comments

--- frontend/src/app/organization/organization-details.component.ts
+++ frontend/src/app/organization/organization-details.component.ts
@@ -5,6 +5,8 @@ import 'rxjs/add/operator/switchMap';
 
 import { OrganizationService } from './organization.service';
 import { environment } from '../../environments/environment';
+import { AuthService } from '../auth.service';
+import { User } from '../user';
 
 @Component({
   selector: 'volontulo-organization-details',
@@ -12,17 +14,24 @@ import { environment } from '../../environments/environment';
   styleUrls: ['./organization-details.component.css'],
 })
 export class OrganizationDetailsComponent implements OnInit {
-
+  //currentUser: User;
   organization$: Observable<Organization>;
   djangoRoot: string = environment.djangoRoot;
 
   constructor(
     private activatedRoute: ActivatedRoute,
-    private organizationService: OrganizationService
-  ) {}
+    private organizationService: OrganizationService,
+    //private authService: AuthService,
+  ) {
+    //this.authService.changeUserEvent
+    //  .subscribe(user => { this.currentUser = user; }
+    //  );
+    //this.currentUser = this.authService.currentUser();
+  }
 
   ngOnInit() {
+    //console.log(this.currentUser.username);
     this.organization$ = this.activatedRoute.params
-      .switchMap(params => this.organizationService.getOrganization(+params.organizationId));
+      .switchMap(params => this.organizationService.getOrganization(params.organizationId));
   }
 }

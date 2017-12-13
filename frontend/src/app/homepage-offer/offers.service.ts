0366e55442195b1cc68ea89639be6d6a372a8591
juliaanholcer <julia.anholcer@interia.pl>
2017-12-13 19:11:08 +0100

    Linter fixing

--- frontend/src/app/homepage-offer/offers.service.ts
+++ frontend/src/app/homepage-offer/offers.service.ts
@@ -1,7 +1,7 @@
 import { Offer } from './offers.model';
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-import { Observable } from "rxjs/Observable";
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 
 import { environment } from '../../environments/environment';

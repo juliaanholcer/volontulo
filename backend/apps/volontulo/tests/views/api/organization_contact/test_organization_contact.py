766d5722f4e20a685433b69dd59ec9bb177b3315
juliaanholcer <julia.anholcer@interia.pl>
2018-01-12 09:54:17 +0100

    Another pycodestyle fixes

--- backend/apps/volontulo/tests/views/api/organization_contact/test_organization_contact.py
+++ backend/apps/volontulo/tests/views/api/organization_contact/test_organization_contact.py
@@ -1,20 +1,27 @@
+"""
+.. module:: test_organization_contact
+"""
 import json
 from unittest import mock
-
 from django.test import TestCase
-
 from apps.volontulo.factories import OrganizationFactory
 
 
 class TestOrganizationContact(TestCase):
+
+    """ Tests for organization contact API """
+
     def setUp(self):
+        """ Set up for each test """
         self.org = OrganizationFactory.create()
 
     def test_404_if_org_does_not_exist(self):
+        """ Test for getting an organization """
         res = self.client.post('/api/organizations/org-5/999/contact')
-        self.assertEquals(res.status_code, 404)
+        self.assertEqual(res.status_code, 404)
 
     def test_400_if_payload_incorrect(self):
+        """ Test for sending incorrect payload"""
         res = self.client.post(
             '/api/organizations/org/{}/contact'.format(self.org.id),
             json.dumps({
@@ -25,14 +32,16 @@ class TestOrganizationContact(TestCase):
             }),
             content_type='application/json'
         )
-        self.assertEquals(res.status_code, 400)
+        self.assertEqual(res.status_code, 400)
 
     def test_405_if_method_not_alowed(self):
+        """ Test for using wrong method """
         res = self.client.get(
             '/api/organizations/org/{}/contact'.format(self.org.id))
-        self.assertEquals(res.status_code, 405)
+        self.assertEqual(res.status_code, 405)
 
     def test_201(self):
+        """ Test for sending correct contact message """
         res = self.client.post(
             '/api/organizations/org/{}/contact'.format(self.org.id),
             json.dumps({
@@ -43,9 +52,10 @@ class TestOrganizationContact(TestCase):
             }),
             content_type='application/json'
         )
-        self.assertEquals(res.status_code, 201)
+        self.assertEqual(res.status_code, 201)
 
     def test_send_email(self):
+        """ Test for sending email """
         with mock.patch('apps.volontulo.views.api.send_mail') as mock_send:
             res = self.client.post(
                 '/api/organizations/org/{}/contact'.format(self.org.id),
@@ -58,5 +68,5 @@ class TestOrganizationContact(TestCase):
                 content_type='application/json'
             )
 
-        self.assertEquals(res.status_code, 201)
-        self.assertEquals(mock_send.call_count, 1)
+        self.assertEqual(res.status_code, 201)
+        self.assertEqual(mock_send.call_count, 1)

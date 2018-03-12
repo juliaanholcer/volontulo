bf8e5d5b6bed25406b81dec6215a0b72f6245747
juliaanholcer <julia.anholcer@interia.pl>
2018-03-12 20:03:08 +0100

    Changes in API

--- backend/apps/volontulo/views/api.py
+++ backend/apps/volontulo/views/api.py
@@ -205,7 +205,6 @@ class OrganizationViewSet(viewsets.ModelViewSet):
     # pylint: disable=invalid-name
     def offers(request, pk):
         """ Endpoint to get offers for organization """
-        import pdb; pdb.set_trace();
         organization = get_object_or_404(Organization, id=pk)
         if logged_as_admin(request) or [up for up in organization.userprofiles.all() if request.user == up.user]:
             offers = organization.offer_set.get_for_administrator()

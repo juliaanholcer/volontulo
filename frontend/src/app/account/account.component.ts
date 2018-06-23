35090e2c12b9f0d7dbe6b9a670a9115951240448
juliaanholcer <julia.anholcer@interia.pl>
2018-06-23 22:45:07 +0200

    add organizations list on account page

--- frontend/src/app/account/account.component.ts
+++ frontend/src/app/account/account.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { map, switchMap } from "rxjs/operators";
+import { pipe } from "rxjs/util/pipe";
+import { AuthService } from "../auth.service";
+import { Organization } from '../organization/organization.model';
 
 @Component({
   selector: 'volontulo-account',
   templateUrl: './account.component.html',
 })
 
-export class AccountComponent {
+export class AccountComponent implements OnInit {
+  organizations$: Observable<any>;
+  constructor(private authService: AuthService) {}
 
+  ngOnInit() {
+    this.organizations$ = this.authService.user$
+      .pipe(
+        map(user => {
+          console.log(user.organizations)
+          return user.organizations
+        })
+      );
+  }
 }
